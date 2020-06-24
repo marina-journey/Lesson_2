4920786eb68e1936cd433b00a9ac3433d1328427
Marina Domrachova <apaycheva@gmail.com>
2020-06-25 01:09:05 +0300

    End of first task

---
+++ script.js
@@ -0,0 +1,19 @@
+let myFirstTry = 'my first try';
+console.log(myFirstTry);
+
+console.log('Hi');
+
+console.warn('Warning!');
+
+console.error('Error');
+
+console.info('Information');
+
+let myNumber = 23;
+console.log(typeof String(myNumber));
+console.log(typeof myNumber);
+
+
+
+
+
